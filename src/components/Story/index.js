aea15c292584b50fa8a4fe4f5632c2a69b85906a
Kailash Sankar <ksankar@conviva.com>
2020-05-31 23:40:29 +0530

    fixed typos

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -10,24 +10,24 @@ function Story() {
       <p>
         <span>TL;DR</span>
         Full stack web developer with over 7.5 years of experience in building
-        applications across perl, python and javascript stacks, developing
+        applications across Perl, Python and JavaScript stacks, developing
         RESTful APIS against SQL and NoSQL databases, analytics systems and
-        building correpsonding user inferfaces. I am proficent in end to end
+        building corresponding user interfaces. I am proficient in end to end
         development, from requirement analysis to design, development and CI/CD.
       </p>
 
       <p>
         <span>More</span>I have a Bachelors degree in Computer Science. After
-        graduation, I started as a backend developer on perl (CGI and Catalyst)
-        framework. Then I picked the vanilla flavours of frontend - HTML, CSS
-        and Javscript. After two years in perl I realized it&apos;s time to pick
-        up another language, Python seemed like an obvious choice. I&apos;ve
-        build apps in Django, MySQL along with Angular.js, Material design lite
-        and Bootstrap.
+        graduation, I started as a backend developer on Perl (CGI and Catalyst)
+        framework. Then I picked the vanilla flavors of frontend - HTML, CSS and
+        JavaScript. After two years in Perl I realized it&apos;s time to pick up
+        another language, Python seemed like an obvious choice. I&apos;ve built
+        apps in Django, MySQL along with Angular.js, Material design lite and
+        Bootstrap.
       </p>
 
       <p>
-        Next I switched my focus on to javascript. I wrote a few plugins in js
+        Next I switched my focus on to JavaScript. I wrote a few plugins in js
         and started working with Node. I worked closely with anlytics teams
         developing APIs which query big data sources like Elasticsearch and
         Cassandra directly.
